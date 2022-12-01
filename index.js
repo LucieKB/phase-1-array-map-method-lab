d7d83a1623d41d9e20fe41af77f448e354aa1fdf
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2022-11-30 21:13:20 -0700

    commit tree 82aaaa671f716a3268455c2f71483273a3da9333

--- index.js
+++ index.js
@@ -11,6 +11,60 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const tutorialsString = tutorials.toString()
+
+const words = tutorialsString.split(" ");  
+
+// function capitalizeFirstLetter(words){
+//   for (let i = 0; i < words.length; i++) {
+//       words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+//   }
+//     return words
+//   }
+  
+//   console.log(capitalizeFirstLetter(words));
+
+// function mapCapitalize(){
+//   return tutorials.map((line)=>{
+//     const splitLine = line.split(" ")
+//     const mapLine = splitLine.map((word)=>{
+//       return word[0].toUpperCase() + word.slice(1);
+//    })
+//    return mapLine
+   
+//   })
+
+  
+// }
+
+function titleCased(){
+  return tutorials.map((line)=>{
+    const splitLine = line.split(" ")
+    const mapLine = splitLine.map((word)=>{
+      return word[0].toUpperCase() + word.slice(1);
+   })
+   const backIntoSentence = mapLine.join(" ")
+   return backIntoSentence
+   
+  })
+
+  
 }
+ 
+// console.log(mapCapitalize())
+
+// function backIntoSentence(){
+//   return mapCapitalize.map((singleWord)=>{
+//     const lineArray = [singleWord]
+//     const joinLine = lineArray.join(" ")
+//    return joinLine
+// })
+// }
+// console.log(backIntoSentence())
+
+// function titleCased(){
+
+//   return backIntoSentence
+// }
+
+// console.log(titleCased())
